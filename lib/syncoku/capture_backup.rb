263a3f3493f3b0ad0590910e47f0659a9ccf3a20
Bill Horsman <bill@logicalcobwebs.com>
2015-09-01 10:03:36 +0100

    Strip the trailing new line of the public URL for backup
    Heroku started adding an extra line to the output so we need to strip
    the whitespace from around the URL. This fixes the error you might have
    got:
    
    curl: (3) Illegal characters found in URL

--- lib/syncoku/capture_backup.rb
+++ lib/syncoku/capture_backup.rb
@@ -5,7 +5,7 @@ module Syncoku
     def capture
       puts "Capturing #{production_app_name} backup..."
       run_on_production("pg:backups capture")
-      run_on_production("pg:backups public-url")
+      run_on_production("pg:backups public-url").strip
     end
 
     def run_on_production(command)
--- lib/syncoku/version.rb
+++ lib/syncoku/version.rb
@@ -1,3 +1,3 @@
 module Syncoku
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
